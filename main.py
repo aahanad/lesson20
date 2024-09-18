6bda990a6c6ce484e2cfeaae5558c88a9235fa4c
aahanad <150878761+aahanad@users.noreply.github.com>
2024-09-17 19:01:07 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import*
+screen=Tk()
+screen.geometry("600x600")
+screen.title("temprature convert🌡")
+screen.config(background="MediumPurple1")
+title=Label(screen,text="temprature converter",bg="CadetBlue1",fg="MediumPurple1",font=("Lola",15,"bold"))
+title.grid(row=1,column=1,padx=200,pady=50)
+screen.mainloop()
+#makelabels and buttons n stuff
